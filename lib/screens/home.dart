96462feb524a22e07b59bb9b4b6a153df6fdd27b
Lavi Kashyap <lavi.bhumca2015@gmail.com>
2021-09-20 18:20:12 +0530

    added BackDrop in Home Screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,13 +1,51 @@
+import 'package:backdrop/backdrop.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_default_code/consts/colors.dart';
+import 'package:ionicons/ionicons.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Home'),
+    return BackdropScaffold(
+      headerHeight: MediaQuery.of(context).size.height * 0.25,
+      appBar: BackdropAppBar(
+        title: Text("Home"),
+        leading: BackdropToggleButton(
+          icon: AnimatedIcons.home_menu,
+        ),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+              ColorsConsts.starterColor,
+              ColorsConsts.endColor
+            ])
+          ),
+        ),
+        actions: <Widget>[
+          IconButton(
+            iconSize: 15,
+            onPressed: (){},
+            padding: const EdgeInsets.all(10),
+            icon: CircleAvatar(
+              radius: 15,
+              backgroundColor: Colors.white,
+              child: CircleAvatar(
+                radius: 13,
+                backgroundImage: NetworkImage(
+                  'https://t3.ftcdn.net/jpg/01/83/55/76/240_F_183557656_DRcvOesmfDl5BIyhPKrcWANFKy2964i9.jpg'
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+      backLayer: Center(
+        child: Text("Back Layer"),
+      ),
+      frontLayer: Center(
+        child: Text("Front Layer"),
       ),
     );
   }
